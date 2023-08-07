c1955a31667aad722e4e54815cb2256305a402e6
TheGodFathers <yusup.kejarid@gmail.com>
2023-08-07 08:31:24 +0700

    update animation with aos

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,40 +41,6 @@ function scrollActive(){
     })
 }
 
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-})
-
-/*SCROLL HOME*/
-sr.reveal('.home__title', {})
-sr.reveal('.home__scroll', {delay: 200})
-// sr.reveal('.home__img', {origin:'right', delay: 200})
-
-/*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 600, interval: 200})
-
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
-
-/*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', {interval: 200})
-
-/*SCROLL CONTACT*/
-sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', {interval: 200})
-sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600})
-
 
 
 
